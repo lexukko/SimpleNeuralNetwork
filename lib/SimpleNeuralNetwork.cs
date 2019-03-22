cf161819f27ecea6aad6371810f510e3aaeb804d
Jose J Lopez <jjld2009@gmail.com>
2019-03-22 17:58:53 -0600

    refactorizacion de clase SimpleNeuralLayer, agregadas excepciones

--- lib/SimpleNeuralNetwork.cs
+++ lib/SimpleNeuralNetwork.cs
@@ -13,83 +13,102 @@ namespace nn.common
 
         public List<SimpleNeuralLayer> layers;
 
-        public SimpleNeuralNetwork(){
+        public SimpleNeuralNetwork() {
             layers = new List<SimpleNeuralLayer>();
         }
 
         // Adds a layer
-        public void add(SimpleNeuralLayer layer){
-            if (layer != null) {
-                layers.Add(layer);
-                if (layers.Count == 1){
-                    this.xs_no = layer.noInputs;
-                    this.ys_no = layer.noOutputs;
-                    this.outputLayerNo = layers.Count - 1;
-                } else if (layers.Count > 1) {
-                    this.ys_no = layer.noOutputs;
-                    this.outputLayerNo = layers.Count - 1;
-                }
+        public void add(SimpleNeuralLayer layer) {
+
+            if (layer == null)
+                throw new System.ArgumentNullException("'layer' no puede ser nulo.");
+
+            layers.Add(layer);
+
+            if (layers.Count == 1) {
+                this.xs_no = layer.noInputs;
+                this.ys_no = layer.noOutputs;
+                this.outputLayerNo = layers.Count - 1;
+            } else if (layers.Count > 1) {
+                this.ys_no = layer.noOutputs;
+                this.outputLayerNo = layers.Count - 1;
             }
-                
+
         }
 
         private void feedforward(float[,] xs) { // Nx1
-            if (xs != null && xs.GetLength(0) == this.xs_no )
+
+            if (xs == null)
+                throw new System.ArgumentNullException("'xs' no puede ser nulo.");
+
+            int nRows_xs = xs.GetLength(0);
+
+            if (nRows_xs != this.xs_no)
+                throw new System.ArgumentException("numero de registros de 'xs' debe ser igual al numero de entradas de la capa.");
+
+            if (this.layers.Count > 0)
             {
-                if (this.layers.Count > 0)
-                {
-                    for(int i = 0; i<this.layers.Count; i++){
-                        if(i == 0){
-                            this.layers[i].set(xs);
-                        } else {
-                            this.layers[i].set(this.layers[i-1].outputs);
-                        }
+                for (int i = 0; i < this.layers.Count; i++) {
+                    if (i == 0) {
+                        this.layers[i].set(xs);
+                    } else {
+                        this.layers[i].set(this.layers[i - 1].outputs);
                     }
                 }
             }
+
         }
+
 
         private void backpropagation(float[,] ys)
         {
-            if ( ys != null && ys.GetLength(0) == this.ys_no)
-            {
-                if (this.layers.Count > 0)
-                {
-                    for (int i = 0; i < this.layers.Count; i++)
-                    {
-                        if (i == 0)
-                        {
+            if (ys == null)
+                throw new System.ArgumentNullException("'ys' no puede ser nulo.");
+
+            int nRows_ys = ys.GetLength(0);
+
+            if (nRows_ys != this.ys_no)
+                throw new System.ArgumentException("numero de registros de 'ys' debe ser igual al numero de entradas de la capa.");
+
+                if (this.layers.Count > 0) {
+                    for (int i = 0; i < this.layers.Count; i++) {
+                        if (i == 0) {
                             
-                        }
-                        else
-                        {
+                        } else { 
                             
                         }
                     }
                 }
-            }
         }
 
         public void train(float[] xs, float [] ys){ // inputs and outputs are matrixes of 1xN
+
+            if (!(this.layers.Count > 0))
+                throw new System.ArgumentException("La red neuronal necesita almenos una capa para poder operar.");
+
             this.feedforward(Matrix.fromArray(xs));
             //this.backpropagation(Matrix.fromArray(ys));
         }
 
         public float[] predict(float[] xs) // predict an output given an input
         {
-            if (this.layers.Count > 0) {
-                this.feedforward(Matrix.fromArray(xs));
-                return Matrix.toArray(this.layers[this.outputLayerNo].outputs);
-            }
-            return null;
+            if (!(this.layers.Count > 0))
+                throw new System.ArgumentException("La red neuronal necesita almenos una capa para poder operar.");
+
+            this.feedforward(Matrix.fromArray(xs));
+            return Matrix.toArray(this.layers[this.outputLayerNo].outputs);
         }
 
         public void save(){ // Save neural network configuration and data (weigths, bias) to a json file maybe to a binary file ( ProtocolBuffers, FlatBuffers)
 
+            if (!(this.layers.Count > 0))
+                throw new System.ArgumentException("La red neuronal necesita almenos una capa para poder operar.");
         }
 
         public void load(){ // load neural network configuration and data (weigths, bias) from a json file maybe from a binary file ( ProtocolBuffers, FlatBuffers)
 
+            if (this.layers.Count > 0)
+                throw new System.ArgumentException("La red neuronal necesita no tener capas para utilizar este metodo.");
         }
 
     }
